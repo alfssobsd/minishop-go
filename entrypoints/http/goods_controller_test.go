8dec02af60754f29559ef3961701d7c207d0c08c
Sergei Kravchuk <alfss.obsd@gmail.com>
2019-06-15 03:12:15 +0300

    [+] fix test TestShowGoodsDetailInfoController

--- entrypoints/http/goods_controller_test.go
+++ entrypoints/http/goods_controller_test.go
@@ -16,14 +16,14 @@ type MockGoodsUC struct {
 	usecases.GoodsUseCase
 }
 
-func (mcu MockGoodsUC) ShowGoodsDetailInfoUseCase(id string) entities.GoodsUseCaseEntity {
-	return entities.GoodsUseCaseEntity{
+func (mcu MockGoodsUC) ShowGoodsDetailInfoUseCase(id string) (*entities.GoodsUseCaseEntity, error) {
+	return &entities.GoodsUseCaseEntity{
 		GoodsId:         "c26e7e02-c1de-465d-88ff-b845abdc47f1",
 		GoodsCodeName:   "0001",
 		GoodsTitle:      "Плющевый медведь",
 		GoodsDescrition: "Милый плющевый медведь",
 		GoodsPrice:      255.5,
-	}
+	}, nil
 }
 
 func TestShowGoodsDetailInfoController(t *testing.T) {
